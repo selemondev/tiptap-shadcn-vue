500b75828db8b0e71fe4f0afcce1697fedcfdfc1
selemondev <selemondev19@gmail.com>
2025-04-08 01:13:49 -0600

    chore: add more deps to the lib dependencies

--- registry/registry-lib.ts
+++ registry/registry-lib.ts
@@ -4,7 +4,7 @@ export const lib: Registry = [
   {
     name: 'utils',
     type: 'registry:lib',
-    dependencies: ['clsx', 'tailwind-merge'],
+    dependencies: ['clsx', 'tailwind-merge', '@atlaskit/pragmatic-drag-and-drop', '@atlaskit/pragmatic-drag-and-drop-hitbox', '@iconify/vue'],
     files: [
       {
         path: 'lib/utils.ts',
